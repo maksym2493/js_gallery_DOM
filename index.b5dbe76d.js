63a5197ad054c0ea78d973f4424c9fa18eacc659
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-22 17:15:33 +0200

    make build

---
+++ index.b5dbe76d.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){var l=t.target.closest(".list-item__link");null!==l&&(e.src=l.href),t.preventDefault()});
+//# sourceMappingURL=index.b5dbe76d.js.map
